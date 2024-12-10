6830e64b81367b1b83e3491d99a53421cc2caa59
Abhijeet2765 <abhijeetdeshmukh.2765@gmail.com>
2024-12-10 12:14:37 +0530

    Basics of Programming Program:file

---
+++ Program.dart
@@ -0,0 +1,13 @@
+import "dart:math";
+
+void main() {
+  String name = "Abhijeet";
+  //String manipulation
+  print(name.toLowerCase());
+  print(name.toUpperCase());
+  print("Hello $name");
+
+  //Maths Function
+  print(sqrt(12));
+  print(min(12, 13));
+}
