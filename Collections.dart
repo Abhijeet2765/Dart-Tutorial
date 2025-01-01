da89dbdf839e71d9e2ef5e7e8b98465a68aab4ff
Abhijeet2765 <abhijeetdeshmukh.2765@gmail.com>
2025-01-01 17:53:53 +0530

    Uploading Program2 and Collections file

---
+++ Collections.dart
@@ -0,0 +1,12 @@
+void main() {
+  var gifts = Map<String, String>();
+  gifts['first'] = 'partridge';
+  gifts['second'] = 'turtledoves';
+  gifts['fifth'] = 'golden rings';
+
+  var nobleGases = Map<int, String>();
+  nobleGases[2] = 'helium';
+  nobleGases[10] = 'neon';
+  nobleGases[18] = 'argon';
+  print(nobleGases[10]);
+}
